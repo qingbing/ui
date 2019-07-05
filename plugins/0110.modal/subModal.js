2adfe9c024d9c1cc710562a96e10841f7421d776
qingbing <qingbing@localhost.localdomain>
2019-07-05 02:53:22 -0400

    modal回调参数增加

--- plugins/0110.modal/subModal.js
+++ plugins/0110.modal/subModal.js
@@ -1,4 +1,5 @@
-function closeCallback() {
+function closeCallback($trigger) {
+    console.log($trigger);
     return {
         rand: H.rand(),
         site: 'phpcorner.net',
@@ -8,21 +9,21 @@ function closeCallback() {
 
 jQuery(function () {
     // 关闭父页面 modal
-    $('.MODAL-CLOSE').click(function (e) {
+    $('.MODAL-CLOSE').on('click', function (e) {
         parent.modal.hide(true);
         H.preventDefault(e);
     });
     // 关闭父页面 modal, 并刷新父页面
-    $('.MODAL-CLOSE-RELOAD').click(function (e) {
+    $('.MODAL-CLOSE-RELOAD').on('click', function (e) {
         parent.modal.hide(false, true);
         H.preventDefault(e);
     });
     // 关闭父页面 modal, 并执行父页面 modal 的回调函数
-    $('.MODAL-CLOSE-CALLBACK').click(function (e) {
-        let _data = $(this).data();
-        if (H.isDefined(_data.callback)) {
-            _data.callback = H.toJson(_data.callback);
-            parent.modal.hide(false, false, _data.callback());
+    $('.MODAL-CLOSE-CALLBACK').on('click', function (e) {
+        let callback = $(this).data('callback');
+        if (H.isDefined(callback)) {
+            callback = H.toJson(callback);
+            parent.modal.hide(false, false, callback($(this)));
         } else {
             parent.modal.hide(true);
         }
